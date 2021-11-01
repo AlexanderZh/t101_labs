399dac94de5453b67c98969f672286ce662d023c
zag <alexander.zhdanov@gmail.com>
2021-11-01 19:50:01 +0300

    lab2

--- lab2/generator.py
+++ lab2/generator.py
@@ -44,8 +44,23 @@ def linear_regression_numpy(filename):
     plt.plot(x, h, "r", label = 'model')    
     plt.legend()
     plt.show()
-    
+    return(model)
 
+def linear_regression_exact(filename):
+    print("Ex1: your code here - exact solution usin invert matrix")
+    return
+    
+def check(model, ground_truth):
+    if len(model) != len(ground_truth):
+        print("Model is inconsistent")
+        return False
+    else:
+        r = np.dot(model-ground_truth,model-ground_truth)/(np.dot(ground_truth,ground_truth))
+        print(r)
+        if r < 0.0001:            
+            return True
+        else:
+            return False
 # Ex1: make the same with polynoms
 
 #generates x and y numpy arrays for 
@@ -65,7 +80,7 @@ def generate_poly(a, n, noise, filename, size = 100):
         y = y + a[i] * np.power(x,i) + noise*(np.random.rand(size, 1) -0.5)
     print(np.shape(x))
     data = np.hstack((x,y))
-    np.savetxt('generated_poly.csv',data,delimiter=',')
+    np.savetxt(filename,data,delimiter=',')
 
 
 def polynomial_regression_numpy(filename):
@@ -76,33 +91,82 @@ def polynomial_regression_numpy(filename):
     print(f"polyfit in {time_end - time_start} seconds")
     
 
+# Ex.2 gradient descent for linear regression without regularization
+
 # find minimum of function J(theta) using gradient descent
 # alpha - speed of descend
 # theta - vector of arguments, we're looking for the optimal ones (shape is 1 х N)
 # J(theta) function which is being minimizing over theta (shape is 1 x 1 - scalar)
-# dJ(theta) partial derivatives of J over theta (shape is 1 x N - the same as theta)
+# dJ(theta) - gradient, i.e. partial derivatives of J over theta - dJ/dtheta_i (shape is 1 x N - the same as theta)
+# x and y are both vectors
 
-def gradient_descent_step(J, theta, alpha):
+def gradient_descent_step(dJ, theta, alpha):
     print("your code goes here")
 
     return(theta)
 
-def get_J(x, y, theta):
-    print("your code goes here")
-    return 0   
+# get gradient over all xy dataset - gradient descent
+def get_dJ(x, y, theta):
+    theta_new = theta
+    print("your code goes here - calculate new theta")
+    return theta_new   
 
-def minimize(J, theta, paha):
+# get gradient over all minibatch of size M of xy dataset - minibatch gradient descent
+def get_dJ_minibatch(x, y, theta, M):
+    theta_new = theta
+    print("your code goes here - calculate new theta")
+    return theta_new     
+
+# get gradient over all minibatch of single sample from xy dataset - stochastic gradient descent
+def get_dJ_sgd(x, y, theta):
+    theta_new = theta
+    print("your code goes here - calculate new theta")
+    return theta_new     
+
+# try each of gradient decsent (complete, minibatch, sgd) for varius alphas
+# L - number of iterations
+# plot results as J(i)
+def minimize(theta, x, y, L):
+    #n - number of samples in learning subset, m - ...
+    n = 12345 # <-- calculate it properly!
+    theta = np.zeros(n) #you can try random initialization
+    dJ = np.zeros(n)
+    for i in range(0,L):
+        theta = get_dJ(x,y,theta) # here you should try different gradient descents
+        J = 0 # here you should calculate it properly
+    #and plot J(i)
     print("your code goes here")
     return
 
 
 if __name__ == "__main__":
-    generate_linear(1,-3,0.5,'linear.csv',100)
-    linear_regression_numpy("linear.csv")
-
+    generate_linear(1,-3,1,'linear.csv',100)
+    model = linear_regression_numpy("linear.csv")
+    print(f"Is model correct?\n{check(model, np.array([1,-3]))}")
+    # ex1 . - exact solution
+    #model_exact = linear_regression_exact("linear.csv")
+    #check(model_exact, np.array([-3,1]))
+    
     #ex1. polynomial with numpy
-    generate_poly([1,2,3],2,0.5)
+    generate_poly([1,2,3],2,0.5, 'polynomial.csv')
     polynomial_regression_numpy("polynomial.csv")
 
-        
+    #ex2. find minimum with gradient descent
+    # 0. generate date with function above
+    # 1. shuffle data into train - test - valid
+    # 2. call minuimize(...) and plot J(i)
+    # 3. call check(theta1, theta2) to check results for optimal theta
+    
+    #ex3. polinomial regression
+    # 0. generate date with function generate_poly for degree=3, use size = 10, 20, 30, ... 100
+    # for each size:
+    # 1. shuffle data into train - test - valid
+    # Now we're going to try different degrees of model to aproximate our data, set degree=1 (linear regression)
+    # 2. call minimize(...) and plot J(i)
+    # 3. call check(theta1, theta2) to check results for optimal theta
+    # 4. plot min(J_train), min(J_test) vs size: is it overfit or underfit?
+    # 
+    # repeat 0-4 for degres = 2,3,4
+
+    # ex3* the same with regularization
     
